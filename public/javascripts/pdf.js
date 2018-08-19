314dd7b42c6edf4e62dcdb7fd3ea4b76679adc78
Pruvost Geoffrey <geoffrey.pruvost62@gmail.com>
2018-08-19 15:05:43 +0200

    implement the viewer

---
+++ public/javascripts/pdf.js
@@ -0,0 +1,102 @@
+var url = 'files/test.pdf';
+
+// Loaded via <script> tag, create shortcut to access PDF.js exports.
+var pdfjsLib = window['pdfjs-dist/build/pdf'];
+
+// The workerSrc property shall be specified.
+pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.0.550/pdf.worker.js';
+
+var pdfDoc = null,
+  pageNum = 1,
+  pageRendering = true,
+  pageNumPending = null,
+  scale = 0.8,
+  canvas = document.getElementById('the-canvas'),
+  ctx = canvas.getContext('2d');
+
+/**
+ * Get page info from document, resize canvas accordingly, and render page.
+* @param num Page number.
+*/
+function renderPage(num, desiredWidth) {
+  pageRendering = true;
+  // Using promise to fetch the page
+  pdfDoc.getPage(num).then(function (page) {
+
+
+    var viewport = page.getViewport(1);
+    var scale = desiredWidth / viewport.width;
+    var viewport = page.getViewport(scale);
+    canvas.height = viewport.height;
+    canvas.width = viewport.width;
+
+    // Render PDF page into canvas context
+    var renderContext = {
+      canvasContext: ctx,
+      viewport: viewport
+    };
+    var renderTask = page.render(renderContext);
+
+    // Wait for rendering to finish
+    renderTask.promise.then(function () {
+      pageRendering = false;
+      if (pageNumPending !== null) {
+        // New page rendering is pending
+        renderPage(pageNumPending, $(".input-width").val());
+        pageNumPending = null;
+      }
+    });
+  });
+
+  // Update page counters
+  document.getElementById('page_num').textContent = num;
+}
+
+/**
+ * If another page rendering in progress, waits until the rendering is
+ * finised. Otherwise, executes rendering immediately.
+ */
+function queueRenderPage(num) {
+  if (pageRendering) {
+    pageNumPending = num;
+  } else {
+    renderPage(num, $(".input-width").val());
+  }
+}
+
+/**
+ * Displays previous page.
+ */
+function onPrevPage() {
+  if (pageNum <= 1) {
+    return;
+  }
+  pageNum--;
+  queueRenderPage(pageNum);
+}
+
+/**
+ * Displays next page.
+ */
+function onNextPage() {
+  if (pageNum >= pdfDoc.numPages) {
+    return;
+  }
+  pageNum++;
+  queueRenderPage(pageNum);
+}
+
+document.getElementById('prev').addEventListener('click', onPrevPage);
+document.getElementById('next').addEventListener('click', onNextPage);
+
+/**
+ * Asynchronously downloads PDF.
+ */
+pdfjsLib.getDocument(url).then(function (pdfDoc_) {
+  pdfDoc = pdfDoc_;
+  document.getElementById('page_count').textContent = pdfDoc.numPages;
+  pdf_width = $(".input-width").val()
+
+  // Initial/first page rendering
+  renderPage(pageNum, pdf_width);
+});
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('pages/index', { title: 'Cloud Reader' });
 });
 
+router.get('/viewer', function(req, res, next) {
+  res.render('pages/viewer', { title: 'Viewer', file: 'files/test.pdf' });
+});
+
 module.exports = router;
