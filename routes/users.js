a44544ead4599efe1f1e692d1a330dd8b195f2a1
Geoffrey Pruvost <geoffrey@pruvost.xyz>
2018-08-25 17:06:10 +0200

    update create account form validation

--- routes/users.js
+++ routes/users.js
@@ -2,12 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/create', function(req, res, next) {
-  res.render('pages/user_register', { title: 'Register' });
+router.get('/create', function (req, res, next) {
+  res.render('pages/user_register', { title: 'Register', error: "" });
 });
 
-router.post('/create', function(req, res, next) {
-res.send("ok");
+router.post('/create', function (req, res, next) {
+  username = req.body.username;
+  mail = req.body.mail;
+  pwd1 = req.body.pwd1;
+  pwd2 = req.body.pwd2;
+
+  if (pwd1 != pwd2) {
+    //res.redirect('/account/create?error=same_pass');
+    res.render('pages/user_register', {
+      title: 'Register',
+      error: "pass_not_same"
+    });
+  }
+
 });
 
 module.exports = router;
